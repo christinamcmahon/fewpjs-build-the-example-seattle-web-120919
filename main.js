9b4b7a8b1f07092281d058cfd3a6532d7d2caa42
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-31 09:12:41 -0800

    Done.

--- main.js
+++ main.js
@@ -4,16 +4,33 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", function () {
+  likeableHearts()
+})
 
+function likeableHearts() {
+  mimicServerCall()
+  document.querySelectorAll('li span').forEach(function (item) {
+    item.addEventListener("click", function () {
+      if (item.innerHTML == FULL_HEART) {
+        item.innerHTML = EMPTY_HEART
+      } else {
+        item.innerHTML = FULL_HEART
+      }
+
+    })
+  })
+
+}
 
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
